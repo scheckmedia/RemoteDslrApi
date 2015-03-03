bb296916e0cc90a2323ecc776feac131c2898097
Tobias Scheck <tobias@scheck-media.de>
2015-03-03 21:59:43 +0100

    server implementation for api and a handler for json responses

--- RemoteDslrApi/json_app.py
+++ RemoteDslrApi/json_app.py
@@ -1,17 +1,20 @@
-from flask import Flask, jsonify    
+from flask import Flask    
 from werkzeug.exceptions import default_exceptions
-from werkzeug.exceptions import HTTPException
+from RemoteDslrApi.camera_controller import CameraController
+from RemoteDslrApi.error import RemoteDslrApiError
 
 __all__ = ['json_app']
-def json_app(import_name, **kwargs):
-    def json_error(ex):
-        response = jsonify(message=str(ex))
-        response.status_code = (ex.code
-                                if isinstance(ex, HTTPException)
-                                else 500)
-        return response
-    
-    app = Flask(import_name, **kwargs)
+
+class Server(Flask):
+    def __init__(self, import_name):        
+        Flask.__init__(self, import_name)
+        self.__camera = CameraController()
+
+    def get_camera(self):
+        return self.__camera
+
+def json_app(import_name, **kwargs):    
+    app = Server(import_name, **kwargs)
     for code in default_exceptions.iterkeys():
-        app.error_handler_spec[None][code] = json_error
+        app.error_handler_spec[None][code] = RemoteDslrApiError.handle
     return app
