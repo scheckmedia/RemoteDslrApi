0d6027fb8e920d6fe9b5d1c1f41ff0666a51959e
Tobias Scheck <tobias@scheck-media.de>
2015-03-03 22:02:34 +0100

    cleanup

--- RemoteDslrApi/routes/status.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, jsonify
-
-status_page = Blueprint('status', __name__)
-@status_page.route('/status', methods=['POST'])
-def get_status():
-    return jsonify({'status' : 'cool'})
