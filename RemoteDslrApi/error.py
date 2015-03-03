ce5eff4d3c6690ba24e297f64cb8c82ddc28dd12
Tobias Scheck <tobias@scheck-media.de>
2015-03-03 22:00:14 +0100

    simple error class which maps gphoto errors to http erros

---
+++ RemoteDslrApi/error.py
@@ -0,0 +1,49 @@
+from flask import jsonify
+from werkzeug.exceptions import HTTPException
+from gphoto2.gphoto2_result import GPhoto2Error
+ERROR_MAP = {
+    "-102" : { "http_code" : 404, "description" : "Data is corrupt. This error is reported by camera drivers if corrupted data has been received that can not be automatically handled. Normally, drivers will do everything possible to automatically recover from this error."},
+    "-103" : { "http_code" : 404, "description" : "An operation failed because a file existed. This error is reported for example when the user tries to create a file that already exists."},
+    "-105" : { "http_code" : 404, "description" : "The specified model could not be found. This error is reported when the user specified a model that does not seem to be supported by any driver."},
+    "-107" : { "http_code" : 404, "description" : "The specified directory could not be found. This error is reported when the user specified a directory that is non-existent."},
+    "-108" : { "http_code" : 404, "description" : "The specified file could not be found. This error is reported when the user wants to access a file that is non-existent."},
+    "-109" : { "http_code" : 404, "description" : "The specified directory already exists. This error is reported for example when the user wants to create a directory that already exists."},
+    "-110" : { "http_code" : 404, "description" : "Camera I/O or a command is in progress."},
+    "-111" : { "http_code" : 404, "description" : "The specified path is not absolute. This error is reported when the user specifies paths that are not absolute, i.e. paths like \"path/to/directory\". As a rule of thumb, in gphoto2, there is nothing like relative paths."},
+    "-112" : { "http_code" : 404, "description" : "A cancellation requestion by the frontend via progress callback and GP_CONTEXT_FEEDBACK_CANCEL was successful and the transfer has been aborted."},
+    "-113" : { "http_code" : 404, "description" : "The camera reported some kind of error. This can be either a photographic error, such as failure to autofocus, underexposure, or violating storage permission, anything else that stops the camera from performing the operation."},
+    "-114" : { "http_code" : 404, "description" : "There was some sort of OS error in communicating with the camera, e.g. lack of permission for an operation."},
+    "-115" : { "http_code" : 404, "description" : "There was not enough free space when uploading a file."},    
+}
+
+class RemoteDslrApiError(Exception):
+    def __init__(self, message, code):        
+        self.message = message
+        self.code = code
+        
+    @staticmethod
+    def handle(ex):
+        response = {}        
+        if(isinstance(ex, GPhoto2Error)) :
+            key = str(ex.code)
+            response["message"] = ex.string 
+            
+            if(ERROR_MAP.has_key(key)):
+                o = ERROR_MAP[key]
+                response["status_code"] = o["http_code"]
+                response["description"] = o["description"]            
+        else:            
+            response["message"] = ex.message
+            
+            if(hasattr(ex, "description")):            
+                response["description"] = ex.description
+
+            code = 500
+            if isinstance(ex, HTTPException) or isinstance(ex, RemoteDslrApiError):
+                code = ex.code
+                                                
+            response["status_code"] = code
+            
+        response["state"] = "fail"
+        return jsonify(response), response["status_code"]
+        
